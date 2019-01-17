3fd3de3b587dffa09403fa6b0f67c4594bb117d7
Eugenia Park <Eugenialpark@gmail.com>
2019-01-16 18:51:21 -0800

    refactor App component into class component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,23 +3,33 @@ import VideoPlayer from './VideoPlayer.js';
 import Search from './Search.js';  //"./" = look in the same directory, "../" = parent dir
 import ExampleVideoData from '../data/exampleVideoData.js';
 
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <Search/>
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+      return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <Search/>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video = {ExampleVideoData[1]}/>
+          </div>
+          <div className="col-md-5">      
+            <VideoList videos = {ExampleVideoData}/>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <VideoPlayer video = {ExampleVideoData[1]}/>
-      </div>
-      <div className="col-md-5">      
-        <VideoList videos = {ExampleVideoData}/>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+
+}
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
