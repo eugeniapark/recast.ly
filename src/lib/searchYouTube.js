87a971d91b41a0f2093b8b817224dbf6f5e31a33
Mina Sorsok <mina.sorsok@gmail.com>
2019-01-17 11:21:17 -0800

    Implement searchYouTube function passing 2 tests

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,34 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  let maxResults = options.max;
+  if (maxResults === undefined) {
+    maxResults = 5;
+  }
+  if (!callback) {
+    return $.ajax({
+      url: 'https://www.googleapis.com/youtube/v3/search',
+      type: 'GET',
+      data: {
+        maxResults: maxResults,
+        q: options.query,
+        key: options.key,
+        part: 'snippet'
+      },
+      contentType: 'application/json'
+    });
+  } else {
+    $.ajax({
+      url: 'https://www.googleapis.com/youtube/v3/search',
+      type: 'GET',
+      data: {
+        maxResults: maxResults,
+        q: options.query,
+        key: options.key,
+        part: 'snippet'
+      },
+      contentType: 'application/json',
+      success: callback
+    });
+  }
 };
 
 export default searchYouTube;
